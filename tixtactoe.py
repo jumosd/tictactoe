c0a54d04df1d68dc3dff0debb6ca45a87249261d
jumosd <jumosd@icloud.com>
2023-05-21 16:10:55 +0900

    틱텍토 게임 완성

--- tixtactoe.py
+++ tixtactoe.py
@@ -14,8 +14,7 @@ class Tic_Tac_Toe:
             for j in range(3):
                 row.append('*')
             self.board.append(row)
-
-        pass
+            
     # 첫 플레이어 선택
     def select_first_player(self):
         if random.randint(0,1) == 0 :
@@ -35,29 +34,33 @@ class Tic_Tac_Toe:
         #행확인
         for i in range(n):
             win = True
-            for j in range(i):
+            for j in range(n):
                 if self.board[i][j] != player:
                     win = False
                     break
             if win == True:
                 return win
+            
         #열확인
         for i in range(n):
             win = True
             for j in range(n):
                 if self.board[j][i] != player:
+                    win = False
                     break
             if win == True:
                 return win
-
+            
         #대각선 확인
+        win = True
         for i in range(n):
-            win = True
-            if self.boare[i][i] != player:
+            if self.board[i][i] != player:
                 win = False
                 break
-            if win == True:
-                return win
+        if win == True:
+            return win
+        
+
         return False
 
     #잔여 빈칸 여부 확인
@@ -103,9 +106,46 @@ class Tic_Tac_Toe:
             if player == "X" :
                 #randint메소드는 1~3까지 로 입력한다 
                 #0을 제와한이유는 사용자와 동일하게 하기위함이다
-                    while True:
-                        row = random.randint(1,3)
-                        col = random.randint(1,3)
-                        if self.board[row][col] =="*":
-                            break
-                    print("컴퓨터가 행"+str(row)+", 열"+str(col)+ "을/를 선택했습니다.")
+                while True:
+                    row = random.randint(1,3)
+                    col = random.randint(1,3)
+                    if self.board[row -1][col -1] == "*":
+                        break
+                print("컴퓨터가 행"+str(row)+", 열"+str(col)+ "을/를 선택했습니다.")
+                print()
+
+            else:
+                row,col =  list(map(int,input("선택한 위치를 입력하세요").split(" ")))
+                print("사용자가 행"+str(row)+", 열"+str(col)+ " 을/를 선택했습니다.")
+                print()
+            
+            # row, col 입력값이 0,0 인경우 게임 종료
+            if row == 0 and col == 0 :
+                break
+            
+            # 입력된 위치표시
+            self.mark_spot(row -1, col -1, player)
+            self.show_board()
+
+            # 현재 플레이어가 이겼는지 확인
+            if self.is_win(player) == True :
+                if player == 'X':
+                    print("컴퓨터가 이겼습니다.")
+                else :
+                    print("사용자가 이겼습니다.")
+                break
+
+            #게임판 가득찼는지 확인
+            if self.is_board_full() ==True:
+                print("무승부 입니다.")
+                break
+
+            #플레이어 변경
+            player = self.next_player(player)
+
+        print()
+        self.show_board()
+
+TTT = Tic_Tac_Toe()
+
+TTT.start()
